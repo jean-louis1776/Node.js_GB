367d7b243fa8b50e893df8f080b0f181eb5ef9ba
ILYA <aleksin095@gmail.com>
2021-11-29 16:37:55 +0300

    hw-3

--- index.js
+++ index.js
@@ -1,54 +1,25 @@
-// date fomat: "november 29 2021 16:33:45"
-var startDates = process.argv.slice(2).map((e) => {
-    return new Date(e).getTime();
+const fs = require("fs");
+const readline = require("readline");
+
+const readStream = fs.createReadStream("./access.log", "utf8");
+
+const file1 = fs.createWriteStream("./89.123.1.41_requests.log");
+const file2 = fs.createWriteStream("./34.48.240.111_requests.log");
+
+//readline module provides an interface for reading data
+
+const read = readline.createInterface({
+    input: readStream,
+    terminal: true,
 });
 
-const EventEmitter = require("events");
-const emitter = new EventEmitter();
-
-function countdown(startDate, i) {
-    let now = new Date().getTime();
-    let end = startDate - now;
-    let days = Math.floor(end / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((end % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((end % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((end % (1000 * 60)) / 1000);
-
-    if (end < 0) {
-        //   clearInterval(tik);
-        console.log("Timer " + (i + 1) + ": Время вышло");
-    } else {
-        console.log(
-            "Timer " +
-            (i + 1) +
-            ": " +
-            days +
-            "д " +
-            hours +
-            "ч " +
-            minutes +
-            "м " +
-            seconds +
-            "с "
-        );
+read.on("line", (line) => {
+    // The includes () method checks if a string contains a given substring and returns true or false, respectively
+    if (line.includes("89.123.1.41")) {
+        file1.write(line + "\n");
     }
-}
 
-emitter.on("test", (startDates) =>
-    startDates.forEach((element, i) => countdown(element, i))
-);
-
-var a = setInterval(() => {
-    const dateObj = new Date();
-    var timers = startDates.length;
-    var end = startDates.length;
-    emitter.emit("test", startDates);
-    startDates.forEach((element) => {
-        if (element >= dateObj.getTime()) {
-            timers = timers - 1;
-        }
-    });
-    if (timers == end) {
-        clearInterval(a);
+    if (line.includes("34.48.240.111")) {
+        file2.write(line + "\n");
     }
-}, 1000);
+});
