24dc3a6fd9ec20ae615560f263817d86160ca879
ILYA <aleksin095@gmail.com>
2021-11-30 14:43:50 +0300

    hw-6

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// ЗАГЛУШКА #
+const socket = require('socket.io');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const indexPath = path.join(__dirname, './index.html');
+    const readStream = fs.createReadStream(indexPath);
+
+    readStream.pipe(res);
+});
+
+const io = socket(server);
+
+io.on('connection', client => {
+    console.log(`clien ${ client.id } connected`);
+    client.on('client-msg', data => {
+        // console.log(data);
+
+        const payload = {
+            message: data.message.split('').reverse().join(''),
+        };
+
+        client.broadcast.emit('server-msg', payload);
+        client.emit('server-msg', payload);
+    })
+});
+
+server.listen(5555);
